5578c3eb7e3cb3e9c97dd89c5915fd3b8c1df7cb
Daria <93216326+DariaaaK@users.noreply.github.com>
2022-01-19 20:56:16 +0200

    Initial commit

---
+++ src/main/java/com/example/liquibase/Liquibase.java
@@ -0,0 +1,13 @@
+package com.example.liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Liquibase {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Liquibase.class, args);
+    }
+
+}
